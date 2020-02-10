473e3a7707b86721ca1db782041d0e274c7ab8f8
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-02-10 18:59:23 +0300

    first commit

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using WebApplication5.Models.Home;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using WebApplication5.Models;
+using WebApplication5.Models.Home;
+using GemBox.Spreadsheet;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFileProvider fileProvider;
+
+        public HomeController(IFileProvider fileProvider)
+        {
+            this.fileProvider = fileProvider;
+        }
+        public IActionResult Index()
+        {
+            //return RedirectToAction("Files");
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+
+            dataTable.Rows.Add(new object[] { 100, "John", "Doe" });
+            dataTable.Rows.Add(new object[] { 101, "Fred", "Nurk" });
+            dataTable.Rows.Add(new object[] { 103, "Hans", "Meier" });
+            dataTable.Rows.Add(new object[] { 104, "Ivan", "Horvat" });
+            dataTable.Rows.Add(new object[] { 105, "Jean", "Dupont" });
+            dataTable.Rows.Add(new object[] { 106, "Mario", "Rossi" });
+            //Excel excel = new Excel();
+            //excel.Export(dataTable);
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            var workbook = new ExcelFile();
+            var worksheet = workbook.Worksheets.Add("DataTable to Sheet");
+
+
+
+            worksheet.Cells[0, 0].Value = "DataTable insert example:";
+
+            // Insert DataTable to an Excel worksheet.
+            worksheet.InsertDataTable(dataTable,
+                new InsertDataTableOptions()
+                {
+                    ColumnHeaders = true,
+                    StartRow = 1
+                });
+
+            workbook.Save($"C:/Users/ivano/source/repos/WebApplication5/WebApplication5/wwwroot/reports/{DateTime.Now.ToString("dd.MM.yyyy.hh.mm.ss")}.xlsx") ;
+            return RedirectToAction("Files");
+
+        }
+
+        public IActionResult Files()
+        {
+            var model = new FilesViewModel();
+            foreach (var item in this.fileProvider.GetDirectoryContents(""))
+            {
+                model.Files.Add(
+                    new FileDetails { Name = item.Name, Path = item.PhysicalPath });
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Download(string filename)
+        {
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(
+                           Directory.GetCurrentDirectory(), "wwwroot", "reports", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
+        }
+    }
+}
---
+++ WebApplication5/Excel.cs
@@ -0,0 +1,34 @@
+﻿using ClosedXML.Excel;
+using GemBox.Spreadsheet;
+using System;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class Excel
+    {
+        public async Task Export(DataTable table)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            var workbook = new ExcelFile();
+            var worksheet = workbook.Worksheets.Add("DataTable to Sheet");
+
+            
+
+            worksheet.Cells[0, 0].Value = "DataTable insert example:";
+
+            // Insert DataTable to an Excel worksheet.
+            worksheet.InsertDataTable(table,
+                new InsertDataTableOptions()
+                {
+                    ColumnHeaders = true,
+                    StartRow = 1
+                });
+
+            workbook.Save("~/wwwroot/reports/DataTable to Sheet.xlsx");
+        }
+    }
+}
